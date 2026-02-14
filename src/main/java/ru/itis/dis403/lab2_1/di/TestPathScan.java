0f0ff9d7d65210e34b0c7fdd4c99db7fcabde0ba
ksenikeev <ksenikeev@mail.ru>
2026-02-14 16:33:07 +0300

    init commit

--- src/main/java/ru/itis/dis403/lab2_1/di/TestPathScan.java
+++ src/main/java/ru/itis/dis403/lab2_1/di/TestPathScan.java
@@ -1,5 +1,6 @@
 package ru.itis.dis403.lab2_1.di;
 
+import ru.itis.dis403.lab2_1.di.config.Context;
 import ru.itis.dis403.lab2_1.di.config.PathScan;
 
 import java.util.List;
@@ -7,7 +8,11 @@ import java.util.List;
 public class TestPathScan {
     public static void main(String[] args) {
 
+/*
         List<Class<?>> classes = PathScan.find("ru.itis.dis403.lab2_1.di");
         classes.forEach(System.out::println);
+*/
+        new Context();
+
     }
 }
--- src/main/java/ru/itis/dis403/lab2_1/di/config/Context.java
+++ src/main/java/ru/itis/dis403/lab2_1/di/config/Context.java
@@ -1,5 +1,7 @@
 package ru.itis.dis403.lab2_1.di.config;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +12,10 @@ public class Context {
 
     private Map<Class<?>, Object> components = new HashMap<>();
 
+    public Context() {
+        scanComponent();
+    }
+
     public Object getComponent(Class clazz) {
         return components.get(clazz);
     }
@@ -18,6 +24,41 @@ public class Context {
         List<Class<?>> classes = PathScan.find(scanPath);
 
         // Создание экземпляров компонент
+        // перебираем список классов
+        // находим конструктор с аргументами, если такого нет - создаем экземпляр
+        // размещаем в components, удаляем из списка
+        // если есть конструктор с аргументами (только компоненты)
+        // пытаемся получить из components объекты - аргументы
+        // если полный набор - создаем экземпляр, иначе пропускаем итерацию
+        int countClasses = classes.size();
+        while (countClasses > 0) {
+            // Перебираем классы компоненты
+            objectNotFound:
+            for (Class c : classes) {
+                if (components.get(c) != null) continue;
+                // берем первый попавшийся контруктор
+                Constructor constructor = c.getConstructors()[0];
+                // извлекаем типы аргументов конструктора
+                Class[] types = constructor.getParameterTypes();
+                // Пытаемся найти готовые компоненты по аргументу
+                Object[] args = new Object[types.length];
+                for (int i = 0; i < types.length; ++i) {
+                    args[i] = components.get(types[i]);
+                    if (args[i] == null) {
+                        continue objectNotFound;
+                    }
+                }
+
+                try {
+                    Object o = constructor.newInstance(args);
+                    components.put(c, o);
+                    countClasses--;
+                    System.out.println(c + " добавлен");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 
 }
